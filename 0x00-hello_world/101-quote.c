4c1510a2c57b9156f72bd9c1d0e0576dcbe5a3d8
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-09-07 13:04:36 -0700

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Discription: A C program that prints with put function
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	const char message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(2, message, 59);
+	return (1);
+}
