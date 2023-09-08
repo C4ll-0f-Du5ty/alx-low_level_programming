8c87d61d88ad87c98234500e2735cb76d42246b9
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-09-08 07:34:38 -0700

    done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Discription: A C program (Positive - negative) function
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char sh = 'a';
+
+	while (sh <= 'z')
+	{
+	if (sh != 'q' && sh != 'e')
+	{
+		putchar(sh);
+	}
+		sh++;
+	}
+	putchar('\n');
+
+	return (0);
+}
