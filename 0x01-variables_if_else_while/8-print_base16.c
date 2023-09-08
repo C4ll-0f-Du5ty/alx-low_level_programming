5d261aa272c9108fc613d93e566ce96ea828121b
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-09-08 08:07:26 -0700

    done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Discription: A C program (Positive - negative) function
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int sh = 0;
+	char ch = 'a'
+
+	while (sh < 10)
+	{
+		putchar(sh + '0');
+		sh++;
+	}
+	while (ch <= 'f')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
