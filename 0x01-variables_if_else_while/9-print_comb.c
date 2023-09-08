e3cb126a66eca97112cf40dd0220c6ecda493910
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-09-08 08:11:29 -0700

    done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Discription: A C program (Positive - negative) function
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int sh = 0;
+
+	while (sh < 10)
+	{
+		putchar(sh + '0');
+		putchar(',');
+		putchar(' ');
+		sh++;
+	}
+	putchar('\n');
+
+	return (0);
+}
