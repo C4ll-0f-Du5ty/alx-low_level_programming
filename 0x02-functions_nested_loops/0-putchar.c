3db7e00033a023a52617494b55110534809b549e
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-09-12 08:24:31 -0700

    done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Discription: A C program (Positive - negative) function
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	putchar("_putchar");
+	return (0);
+}
