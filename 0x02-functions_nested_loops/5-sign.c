e5c27899a3ed0ef695a47ff19bab63a8cb8e25a6
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-09-12 09:52:40 -0700

    done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,17 +17,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		put("+");
+		puts("+");
 		return (1);
 	}
 	if (n == 0)
 	{
-		put("0");
+		puts("0");
 		return (0);
 	}
 	if (n < 0)
 	{
-		put("-");
+		puts("-");
 		return (-1);
 	}
 	return (0);
