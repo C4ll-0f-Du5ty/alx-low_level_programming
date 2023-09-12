9e1cbcea7fe1eb92b497acdaf96acb5491eab6c9
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-09-12 09:03:10 -0700

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Discription: A C program (Positive - negative) function
+ *
+ * Return: always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char sh = 'a';
+
+	while (sh <= 'z')
+	{
+		_putchar(sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
