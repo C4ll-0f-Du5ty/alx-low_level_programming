a846268da27150c63159bacd364aeb3a0da032ad
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-09-14 06:24:30 -0700

    done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - check for a digit
+ * @n : number of \\ to be printed
+ * Return:void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar(' ');
+	_putchar('\\');
+	_putchar('\n');
+}
