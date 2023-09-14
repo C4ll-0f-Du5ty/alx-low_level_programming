93fa962f3d085de3ed4048e50712dd4730113aed
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-09-14 04:53:58 -0700

    done

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -10,11 +10,7 @@
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
-		return (1);
-	else if (c >= 'a' && c <= 'f')
-		return (1);
-	else if (c >= 'A' && c <= 'F')
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
