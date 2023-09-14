d57ac4c319a2930e30f1bedf82ae95d435b8df54
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-09-14 09:10:06 -0700

    done

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,5 +9,19 @@
 
 void print_number(int n)
 {
-	printf("%i", n);
+unsigned int s = n;
+
+	/*first check if its negative*/
+	if (n < 0)
+	{
+		_putchar('-');
+		s = -s;
+	}
+
+	/*print the first few digits*/
+	if ((s / 10) > 0)
+		print_number(s / 10);
+
+	/*print the last digit*/
+	_putchar((s % 10) + 48);
 }
