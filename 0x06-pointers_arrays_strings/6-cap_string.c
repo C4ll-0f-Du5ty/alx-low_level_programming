bb905e3975111be8a488389183e80a4f3d63f0f9
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-09-21 00:13:24 -0700

    done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,6 +3,17 @@
 #include <string.h>
 
 /**
+ * isLower - finds the lower characters in ascii forms
+ * @c: the character
+ * Return: the answer
+ */
+
+int isLower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
  * cap_string - capitalizes all words of a string
  * @str: input string
  * Return: string with capitalized words
@@ -11,13 +22,23 @@
 char *cap_string(char *str)
 {
 	int j = 0;
+	char delimiter[] = " \t\n,.!?\"(){}";
+	int flag = 1;
 
 	while (str[j] != '\0')
 	{
-	while (str[j] >= 97 && str[j] <= 122)
+	if (isLower(str[j] && flag))
 	{
 		str[j] = str[j] - 32;
-
+		flag = 0;
+	}
+	for (i = 0; i < 12; i++)
+	{
+		if (c == delimiter[i])
+		{
+			flag = 1;
+			break;
+	}
 	}
 	j++;
 	}
