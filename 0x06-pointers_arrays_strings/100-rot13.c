05efdde6c3c275d817eca0b1dc8e3b2bc411f429
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-09-21 10:52:29 -0700

    done

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,20 +8,24 @@
  * Return: the encoded ptr
  */
 
-char *rot13(char *ptr) {
+char *rot13(char *ptr)
+{
 	int i;
 
 	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (*ptr) {
-		for (i = 0; i <= 52; i++) {
-			if (*ptr == rot13[i]) {
+	while (*ptr)
+	{
+		for (i = 0; i <= 52; i++)
+		{
+			if (*ptr == rot13[i])
+			{
 				*ptr = ROT13[i];
 				break;
 			}
 		}
 		ptr++;
 	}
-	return ptr;
+	return (ptr);
 }
