d0b0a8d8368f1fb2c765ec988b878a3c07e5172f
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-09-28 01:24:20 -0700

    done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,13 +9,11 @@
  * Return: 0 or 1
  */
 
-int p(char *s, int i)
+int p(char *s, int i, int n)
 {
-	int n = strlen(s);
-
 	if (i < n / 2 && s[i] == s[n - 1 - i])
-		return (p(char s, i + 1));
-	else if (s[i] == s[n - 1 - i])
+		return (p(s, i + 1));
+	else if (i >= n / 2 && s[i] == s[n - 1 - i])
 		return (1);
 	else
 		return (0);
@@ -33,7 +31,7 @@ int is_palindrome(char *s)
 	int n = strlen(s);
 
 	if (s[i] == s[n - 1 - i])
-		return (p(char s, i + 1));
+		return (p(s, i + 1, n));
 	else
 		return (0);
 }
