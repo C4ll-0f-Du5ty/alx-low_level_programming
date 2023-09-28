3182438fae37d8cc11867888704e024772af9bdf
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-09-28 00:33:52 -0700

    done

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * power - find natural square root
+ * @n: int
+ * @i: square root
+ * Return: int
+ */
+
+int power(int n, int i)
+{
+	if (i * i < n)
+	return (power(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - find natural square root
+ * @n: int
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n % 2 == 0 || n % 5 == 0)
+		return (power(n, 1));
+	else
+		return (-1);
+}
