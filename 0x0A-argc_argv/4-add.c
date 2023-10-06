e5535a99128025b0a6ab7dbfec58fb3f15ee98b1
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-10-06 00:28:49 -0700

    done

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - prints program name, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i, j;
+
+	if (argc == 1)
+	{
+	printf("0\n");
+	return (1);
+	}
+
+	for (i = 0; argv[i] != '\0'; i++)
+	{
+		if (isdigit(argv[i]))
+		{
+		j += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%i\n", j);
+	return (0);
+}
