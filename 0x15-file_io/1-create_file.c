f2dcc3cc03a104e46be7f4c0d24994f561b88e7c
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-11-01 23:08:02 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,6 +19,10 @@ int create_file(const char *filename, char *text_content)
 	i = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (i == -1)
 		return (-1);
+	if (chmod(filename, S_IRUSR | S_IWUSR) != 0)
+	{
+	return -1;
+	}
 	if (len)
 		bytes = write(i, text_content, len);
 	close(i);
