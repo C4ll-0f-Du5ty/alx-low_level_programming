e62a00272aea1cec88af6383d3df832b57940bb7
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-11-01 21:55:23 -0700

    done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,47 +8,40 @@
  * Return: number bytes read/printed
  */
 
-ssize_t read_textfile(const char *filename, size_t letters) {
+ssize_t read_textfile(const char *filename, size_t letters)
+{
 	ssize_t bytes_read;
 	char *buffer;
 	FILE *file;
 	ssize_t bytes_written;
 
-	if (filename == NULL) {
+	if (filename == NULL)
+	{
 		return (0);
 	}
-
 	file = fopen(filename, "r");
-	if (file == NULL) {
-		return (0);
-	}
-
+	if (file == NULL)
+	{
+		return (0); }
 	buffer = (char *)malloc(letters + 1);
-
-	if (buffer == NULL) {
+	if (buffer == NULL)
+	{
 		fclose(file);
-		return (0);
-	}
-
+		return (0); }
 	bytes_read = fread(buffer, 1, letters, file);
-
-	if (bytes_read < 0) {
+	if (bytes_read < 0)
+	{
 		free(buffer);
 		fclose(file);
-		return (0);
-	}
-
+		return (0); }
 	buffer[bytes_read] = '\0';
-
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written < 0 || bytes_written != bytes_read) {
+	if (bytes_written < 0 || bytes_written != bytes_read)
+	{
 		free(buffer);
 		fclose(file);
-		return (0);
-	}
-
+		return (0); }
 	free(buffer);
 	fclose(file);
-
 	return (bytes_read);
 }
