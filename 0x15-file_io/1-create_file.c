80465ae4576fcf748d8341a9b5cece98477b7210
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-11-01 23:04:09 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	i = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+	i = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (i == -1)
 		return (-1);
 	if (len)
