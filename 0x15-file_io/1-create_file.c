fe6734f603ea75d48082d0a6df175fd7ad9b0854
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-11-01 23:28:06 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -36,7 +36,7 @@ int create_file(const char *filename, char *text_content)
 	if (stat(filename, &st) == 0)
 	{
 		file = fopen(filename, "w");
-		if (file == NULL)
+		if (file == NULL
 			return (-1); /* Return -1 if the file */
 
 		fclose(file);
