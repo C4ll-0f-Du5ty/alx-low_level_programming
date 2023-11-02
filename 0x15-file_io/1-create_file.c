e4f886f32e62e3cfbcd51b779b5947ae52a5bfc2
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-11-01 23:29:28 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,48 +1,42 @@
 #include "main.h"
 
 /**
- * create_file - Creates or truncates a file with specified content.
- * @filename: The name of the file to create.
- * @text_content: A NULL-terminated string to write (can be NULL).
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
  *
- * Return: 1 on success, -1 on failure (file cannot be created).
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content: text to write
+ *
+ * Return: 1 on success 0 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	struct stat st;
-	size_t text_length;
-	FILE *file;
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
 
-	if (filename == NULL)
-		return (-1); /* Return -1 if filename is NULL */
-
-	file = fopen(filename, "w");
-
-	if (file == NULL)
-		return (-1); /* Return -1 if the file cannot be created */
-
-	if (text_content != NULL)
-	{
-		text_length = strlen(text_content);
-
-		if (fwrite(text_content, 1, text_length, file) != text_length)
-		{
-			fclose(file);
-			return (-1); /* Return -1 if the write operation fails */
-		}
-	}
-	fclose(file);
-	/* Set file permissions to rw------- if the file is newly created */
-	if (stat(filename, &st) == 0)
-	{
-		file = fopen(filename, "w");
-		if (file == NULL
-			return (-1); /* Return -1 if the file */
-
-		fclose(file);
-		return (1); /* File already exists and has been truncated */
-	}
-	if (chmod(filename, S_IRUSR | S_IWUSR) != 0)
-		return (-1); /* Return -1 if changing permissions fails */
-	return (1); /* Return 1 on success */
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
 }
