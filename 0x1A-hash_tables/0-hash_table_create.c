f04e88926061230e239bb9fae15af6c04ea976a3
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-12-21 21:11:49 +0200

    done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,13 +4,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *number1;
 
-    number1->size = size;
-    number1->array = calloc(size, sizeof(hash_table_t));
+    number1 = malloc(sizeof(hash_table_t));
     if (!number1)
     {
         free(number1);
         return 0;
     }
-
+    number1->size = size;
+    number1->array = calloc(size, sizeof(hash_table_t));
     return (number1);
 }
