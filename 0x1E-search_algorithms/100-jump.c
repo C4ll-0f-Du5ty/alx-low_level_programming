1f988d38f43d8054f77e89ff796e28954539ce46
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-04-12 00:04:37 +0200

    done

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int jump_search(int *array, size_t size, int value)
+{
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,6 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
-
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
@@ -16,9 +15,8 @@
 #include <dirent.h>
 #include <signal.h>
 
-
-
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif
