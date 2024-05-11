fed303fb92148953db2e3e04876c1921b6cda87e
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-05-11 20:57:11 +0300

    done

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,29 +12,32 @@
  * Return: The first index where value is located, or -1 if value is not
  *         present or array is NULL
  */
+
 int interpolation_search(int *array, size_t size, int value)
 {
-    size_t low, high;
+	size_t low, high;
 
-    if (array == NULL || size == 0)
-        return -1;
+	if (array == NULL || size == 0)
+		return (-1);
 
-    low = 0, high = size - 1;
+	low = 0, high = size - 1;
 
-    while (low <= high && value >= array[low] && value <= array[high])
-    {
-        size_t pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
-        printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+	while (low <= high && value >= array[low] && value <= array[high])
+	{
+		size_t pos = low + (((double)(high - low) /
+							 (array[high] - array[low])) *
+							(value - array[low]));
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
-        if (array[pos] == value)
-            return pos;
+		if (array[pos] == value)
+			return (pos);
 
-        if (array[pos] < value)
-            low = pos + 1;
-        else
-            high = pos - 1;
-    }
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+	}
 
-    printf("Value checked array[%lu] is out of range\n", low);
-    return -1;
+	printf("Value checked array[%lu] is out of range\n", low);
+	return (-1);
 }
--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -14,36 +14,36 @@
  */
 int exponential_search(int *array, size_t size, int value)
 {
-    size_t bound = 1;
-    size_t low, high;
+	size_t bound = 1;
+	size_t low, high;
 
-    if (array == NULL || size == 0)
-        return -1;
+	if (array == NULL || size == 0)
+		return (-1);
 
-    while (bound < size && array[bound] < value)
-    {
-        printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
-        bound *= 2;
-    }
+	while (bound < size && array[bound] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
+		bound *= 2;
+	}
 
-    low = bound / 2;
-    high = (bound < size - 1) ? bound : size - 1;
+	low = bound / 2;
+	high = (bound < size - 1) ? bound : size - 1;
 
-    printf("Value found between indexes [%lu] and [%lu]\n", low, high);
-    printf("Searching in array: ");
-    for (size_t i = low; i <= high; i++)
-    {
-        if (i > low)
-            printf(", ");
-        printf("%d", array[i]);
-    }
-    printf("\n");
+	printf("Value found between indexes [%lu] and [%lu]\n", low, high);
+	printf("Searching in array: ");
+	for (size_t i = low; i <= high; i++)
+	{
+		if (i > low)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
 
-    return binary_search_(array, low, high, value);
+	return (binary_search_(array, low, high, value));
 }
 
 /**
- * binary_search - Searches for a value in a sorted array of integers
+ * binary_search_ - Searches for a value in a sorted array of integers
  *                 using the Binary search algorithm
  * @array: Pointer to the first element of the array to search in
  * @low: Index representing the lower bound of the search range
@@ -55,26 +55,26 @@ int exponential_search(int *array, size_t size, int value)
  */
 int binary_search_(int *array, size_t low, size_t high, int value)
 {
-    size_t mid;
+	size_t mid;
 
-    while (low <= high)
-    {
-        mid = (low + high) / 2;
-        printf("Searching in array: ");
-        for (size_t i = low; i <= high; i++)
-        {
-            if (i > low)
-                printf(", ");
-            printf("%d", array[i]);
-        }
-        printf("\n");
-        if (array[mid] == value)
-            return mid;
-        else if (array[mid] < value)
-            low = mid + 1;
-        else
-            high = mid - 1;
-    }
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		printf("Searching in array: ");
+		for (size_t i = low; i <= high; i++)
+		{
+			if (i > low)
+				printf(", ");
+			printf("%d", array[i]);
+		}
+		printf("\n");
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
 
-    return -1;
+	return (-1);
 }
